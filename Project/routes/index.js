15cb1d19afe1a0caadd34dfe651b5ac4414a2040
HyoHyun <doch2130@gmail.com>
2022-12-17 14:59:07 +0900

    hyo_merge_1st

--- Project/routes/index.js
+++ Project/routes/index.js
@@ -1,7 +1,6 @@
-// 샘플용
-
 const express = require("express");
 const controllerUser = require("../controller/Cuser");
+const controllerChart = require("../controller/Cchart");
 const router = express.Router();
 
 router.get("/", controllerUser.main);
@@ -9,5 +8,9 @@ router.get("/login", controllerUser.loginPage);
 router.post("/login", controllerUser.login);
 router.delete("/logout", controllerUser.logout);
 
+
+// 멜론 차트 페이지
+router.get("/youtubeRealChart", controllerChart.youtubeRealChartMain);
+router.get("/youtubeRealChart/:num", controllerChart.youtubeRealChartMainType);
 
 module.exports = router;
