d40825a4b8c04816fae97510b41c63a4a3994cb4
HyoHyun <doch2130@gmail.com>
2022-12-21 15:14:43 +0900

    feat: 2022-12-21 1514 hyo_arum_routeindex_merge

--- Project/routes/index.js
+++ Project/routes/index.js
@@ -4,7 +4,6 @@ const controllerUser = require("../controller/Cuser");
 const controllerChart = require("../controller/Cchart");
 const controllerCrawling = require("../controller/Ccrawling");
 const router = express.Router();
-
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs')
@@ -19,19 +18,15 @@ router.get('/signup', controllerUser.register);
 router.post('/signup', controllerUser.post_signup);
 // 로그아웃
 router.delete('/logout', controllerUser.user_logout);
-
 // 회원가입 중복 검사
 router.post('/check_id', controllerUser.check_id);
 router.post('/check_name', controllerUser.check_name);
 router.post('/check_mail', controllerUser.check_mail);
-
 // 개인정보 수정
 router.get('/Edit_info', controllerUser.Edit_info);
 router.patch('/Edit_info_update', controllerUser.Edit_info_update);
-
 // 회원 탈퇴
 router.delete('/user_delete', controllerUser.user_delete);
-
 // 마이 페이지
 router.get('/mypage', controllerUser.mypage);
 
@@ -53,12 +48,18 @@ router.post('/upload_file', upload.single('img'), controllerUser.upload_file);
 
 // 차트 모아보기 페이지
 router.get("/allChart", controllerChart.allChart);
-
 // 유튜브 차트 페이지
 router.get("/youtubeRealChart", controllerChart.youtubeRealChartMain);
 router.get("/youtubeRealChart/:num", controllerChart.youtubeRealChartMainType);
 
 
+// 크롤링 요청 페이지
+router.get("/crawling/melon", controllerCrawling.melonCrawlingPage);
+router.get("/crawling/melonday", controllerCrawling.melonDayCrawlingPage);
+router.get("/crawling/genie", controllerCrawling.genieCrawlingPage);
+router.get("/crawling/geniemovie", controllerCrawling.genieMovieCrawlingPage);
+router.get("/crawling/youtube", controllerCrawling.youtubeCrawlingPage);
+router.get("/crawling/youtubemovie", controllerCrawling.youtubeMovieCrawlingPage);
 
 
 module.exports = router;
